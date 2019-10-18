503769a4085dc902656ed55af8ec8c7752b76b90
liana <hilllia@ssfs.org>
2019-10-18 12:06:10 -0400

    started unit five exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -2,6 +2,16 @@
 # last updated October 18, 2019
 # daily exercises - unit 5
 
-# number one
-print("Please pick numbers that make sense")
-user_upper = int(input("Enter a number"))
+# # number one
+# print("Please pick numbers that make sense")
+# user_upper = int(input("Enter a number"))
+
+# number two
+for x in range(10, 21):
+    print(x)
+
+for x in range(2, 42, 2):
+    print(x)
+
+# problem three
+user_positive_integer = int(input("Please enter a positive number"))
