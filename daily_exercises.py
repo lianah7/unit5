835dbe91ef8d00c1f4a2f17dcc4f835651a19026
liana <hilllia@ssfs.org>
2019-10-24 08:59:35 -0400

    worked on unit five daily exercises

--- daily_exercises.py
+++ daily_exercises.py
@@ -2,16 +2,35 @@
 # last updated October 18, 2019
 # daily exercises - unit 5
 
+import random
+
 # # number one
+# user_upper - 1 includes the chosen value as it counts down
 # print("Please pick numbers that make sense")
-# user_upper = int(input("Enter a number"))
+# user_upper = int(input("Enter a number for the upper value"))
+# user_lower = int(input("Please enter a value for the lower value"))
+# for x in range(user_lower, user_upper + 1):
+#     print(x)
+# if user_upper < user_lower:
+#     for x in range(user_lower, user_upper - 1, -1):
+#         print(x)
 
-# number two
-for x in range(10, 21):
-    print(x)
+# # number two
+# for x in range(10, 21):
+#     print(x)
+#
+# for x in range(2, 42, 2):
+#     print(x)
 
-for x in range(2, 42, 2):
-    print(x)
+# # problem three
+# user_positive_integer = int(input("Please enter a positive number"))
+# for x in range(13):
+#     print(user_positive_integer, '*', x, '=', user_positive_integer * x)
 
-# problem three
-user_positive_integer = int(input("Please enter a positive number"))
+# number four
+for x in range(10):
+    print(random.randint(1, 100))
+
+
+
+
