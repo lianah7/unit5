add8c7e309415ebb1cc2f133cb5bfebea2ac2184
liana <hilllia@ssfs.org>
2019-10-25 10:17:10 -0400

    worked on unit five daily exercises - problems five through seven

--- daily_exercises.py
+++ daily_exercises.py
@@ -27,9 +27,46 @@ import random
 # for x in range(13):
 #     print(user_positive_integer, '*', x, '=', user_positive_integer * x)
 
-# number four
-for x in range(10):
-    print(random.randint(1, 100))
+# # number four
+# even_sum = 0
+# odd_sum = 0
+# for x in range(10):
+#     random_number = (random.randint(1, 100))
+#     print(random_number)
+#     if random_number % 2 == 0:
+#         even_sum = even_sum + random_number
+#     else:
+#         odd_sum = odd_sum + random_number
+# print("The sum of the even numbers is", even_sum)
+# print("The sum of the odd numbers is", odd_sum)
+
+# # number five
+# multiples = 0
+# for x in range(3, 1000):
+#     if x % 3 == 0 or x % 5 == 0:
+#         multiples = multiples + x
+# print("The sum of all the multiples of three and five is", multiples)
+
+# # number six
+# user_num =  int(input("Please pick a number to make a right triangle"))
+# number_stars = "* "
+# for x in range(user_num + 1):
+#     print(number_stars * x)
+
+# number seven
+user_int = (input("Please enter a number"))
+while True:
+    if user_int = "q":
+
+
+
+
+# number eight
+
+
+
+
+
 
 
 
